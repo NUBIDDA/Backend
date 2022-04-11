90297b263bf0db780636472537b08d28a0523711
hyunlee214 <hyun110214@naver.com>
2022-04-11 21:53:45 +0900

    [프로젝트]: 구조 셋팅 및 기능추가 - 구조 개편 (API 구조) - 회원 가입 기능 추가

---
+++ api/user.js
@@ -0,0 +1,46 @@
+const resp = require("../service/response");
+
+const {
+  User,
+  Sequelize: { Op },
+} = require("../models");
+
+
+const Join = async (req, res, next) => {
+  const {
+    email,
+    password,
+  } = req.body;
+
+  let user;
+  try {
+    const existUserEmail = await User.findOne({
+      where: { [Op.or]: [{ email }] },
+    });
+    if (existUserEmail) {
+      return resp(res, 409, { msg: "이미 존재하는 회원입니다" });
+    };
+
+    if (!email || !password) {
+      return resp(res, 400, { msg: "빈칸을 채워주세요" });
+    };
+
+    user = await User.create({
+      email,
+      password,
+    });
+
+    if (user) {
+      return resp(res, 200, { msg: "가입 완료" });
+    }
+
+    return resp(res, 404, { msg: "잘못된 접근입니다" });
+  } catch (e) {
+    return next(e);
+  }
+};
+
+
+module.exports = {
+  Join,
+}
